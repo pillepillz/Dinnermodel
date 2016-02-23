64a1ad8924d3cdeb28e8bf305fdc39f4801d325d
pillepillz <phigul@kth.se>
2016-02-23 12:53:52 +0100

    Added files via upload

---
+++ dinnerplanner-html-master/js/view/exampleView3Controller.js
@@ -0,0 +1,23 @@
+var ExampleView3Controller = function(view, model) {
+
+	view.plusButton.click(function(){
+		model.setNumberOfGuests(model.getNumberOfGuests()+1);
+	});
+
+	view.minusButton.click(function(){
+		if(model.getNumberOfGuests()>0){
+			model.setNumberOfGuests(model.getNumberOfGuests()-1);
+		}
+	
+	});
+
+	view.backbutton.click(function(){
+		$("#exampleView2").show();
+		$("#exampleView3").hide();
+	});
+	
+	view.getThatDish.click(function(){
+		view.type = "starter";
+	
+	});
+}
